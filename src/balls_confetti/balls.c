fe78aa56d476e7618756b4a4bd45593a4af6a81f
LinuxBeaver <jaydc@gmx.us>
2025-06-12 17:50:48 -0400

    removing pippins leftover 2006 copyright

--- src/balls_confetti/balls.c
+++ src/balls_confetti/balls.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
  *
- * Credit to Øvind Kolas (pippin) for major GEGL contributions
+ * Credit to Øyvind Kolås (pippin) for major GEGL contributions
  * 2023 Beaver - Gum Balls Gimp Plugin
 
 Graph here to test plugin without installing paste this syntax inside Gimp's GEGL Graph plugin.
